b95802db652d2986655d03751d47225c718d3dd8
FAT CodeSmith <fahimasfaq01@gmail.com>
2025-07-11 00:23:20 +0600

    Create 1059 - Even Numbers.java

---
+++ Java/1059 - Even Numbers.java
@@ -0,0 +1,10 @@
+public class Main{
+    public static void main (String [] args){
+         for(int i = 1; i <= 100; i++){
+            int a = i % 2;
+            if (a == 0){
+                System.out.println(i);
+            }
+         }
+    }
+}
